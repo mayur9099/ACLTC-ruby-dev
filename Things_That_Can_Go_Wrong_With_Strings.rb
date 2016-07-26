240ec3e342048e0990e2182e7fe6919d055cc86a
Mayur Narang <m_narang@apple.com>
2016-07-25 21:53:53 -0700

    solutions

---
+++ Things_That_Can_Go_Wrong_With_Strings.rb
@@ -0,0 +1,8 @@
+#Debug: Fix the code to get the desired output:
+#puts 12 * "4"                   # should produce => 48
+
+puts 12 * 4 
+
+#puts 3 + " times a day"         # should produce => 3 times a day
+
+puts 3.to_s + " times a day"      
