02db51a3ebfebdc78faf69d24a7e1d4829bca6b9
Mayur Narang <m_narang@apple.com>
2016-07-25 21:34:28 -0700

    Solutions

---
+++ code_flow_top_down.rb
@@ -0,0 +1,35 @@
+#Create a ruby program that outputs:
+#8
+#6
+#7
+#5	
+#3
+#0
+#9
+puts 8
+puts 6
+puts 7
+puts 5
+puts 3
+puts 0
+puts 9
+
+#exercise 2
+
+#Look at this code:
+#puts 3
+#puts 1
+#put 4
+#puts 1
+#puts 5
+#puts 9
+#If you run the code, why do you get an error?
+
+#Solution
+
+puts 3
+puts 1
+puts 4
+puts 1
+puts 5
+puts 9
---
+++ intro_to_ruby.rb
@@ -0,0 +1,19 @@
+#Create a brand new Ruby file that displays the result of some subtraction and run it from the terminal. 
+#What happens if you subtract a greater number from a lesser number?
+x=10
+y=5
+c=3
+d=12
+puts z=x-y
+puts b=c-d
+#Look at this code:
+#PUTS 3 + 3
+#Predict what exactly will happen when you run it. Then run it and see if you were correct.
+puts 3+3
+#Create another ruby file called many_ways.rb. You could get the number 10 to appear in the terminal (the output) by writing the code:
+#puts 7 + 3
+#Write code 3 more ways that will also output the number 10 in the terminal.
+puts 7 + 3
+puts 5+5
+puts 8+2
+puts 6+4
