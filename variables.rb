b3416a9bf9b836cf494c75f8bc09005b1f99f71f
Mayur Narang <m_narang@apple.com>
2016-07-25 22:11:22 -0700

    solutions

---
+++ variables.rb
@@ -0,0 +1,53 @@
+#Predict what happens when you run the following code:
+#a = 1
+#b = 2
+#c = a + b
+#d = c + a + b
+#puts d
+
+a = 1
+b = 2
+c = a + b
+d = c + a + b
+puts d
+
+#Answer -> 6
+
+
+#Run the following code - you should see an error message. Fix the bug.
+hello = "goodnight"
+world = " moon"
+puts hello + world
+hello = "cat"
+world = "fish"
+puts hello + world
+x = 1
+y = 2
+puts x + y
+
+#solution -> #goodnight moon
+#catfish
+#3
+
+#What does the following code do:
+a = 1
+b = a
+puts b
+
+#solution -> 1
+
+
+
+#What does the following code do:
+#a = 1
+#b = a
+#a = 2
+#puts b
+#Why do you think that is?
+
+a = 1
+b = a
+a = 2
+puts b
+
+#solution -> 1
