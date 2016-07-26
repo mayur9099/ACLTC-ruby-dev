a2879c698d6d6fbc1cc4cf53ca5f2cc746abb71a
Mayur Narang <m_narang@apple.com>
2016-07-25 21:23:48 -0700

    exercise 1 solutions

---
+++ exercise_1.rb
@@ -0,0 +1,8 @@
+#Create a brand new Ruby file that displays the result of some subtraction and run it from the terminal. 
+#What happens if you subtract a greater number from a lesser number?
+x=10
+y=5
+c=3
+d=12
+puts z=x-y
+puts b=c-d
